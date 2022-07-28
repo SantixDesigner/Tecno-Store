7e93478253dc5f2cfdf753e9f737dbf5d7cd794a
Santi <xxsmating@gmail.com>
2022-07-28 09:17:43 -0300

    reemplace los viejos id por una sola clase, así ahorramos código y utilizamos values

--- scripts/compra.js
+++ scripts/compra.js
@@ -25,20 +25,10 @@ total.innerHTML = `
 <p>Productos totales: ${cantity}</p>`;
 buyZ.innerHTML = content;
 buyZ.append(total);
-
-let installments1 = document.getElementById('1installment');
-let installments3 = document.getElementById('3installments');
-let installments6 = document.getElementById('6installments');
-let installments12 = document.getElementById('12installments');
-let installments18 = document.getElementById('18installments');
-let installments30 = document.getElementById('30installments');
-installments1.innerHTML = `Paga en una cuota de u$d${Math.round((precioFinal) * 1.21)}`;
-installments3.innerHTML = `Paga en 3 cuotas de u$d${(Math.round((precioFinal / 3) * 1.21))}`;
-installments6.innerHTML = `Paga en 6 cuotas de u$d${(Math.round((precioFinal / 6) * 1.21))}`;
-installments12.innerHTML = `Paga en 12 cuotas de u$d${(Math.round((precioFinal / 12) * 1.21))}`;
-installments18.innerHTML = `Paga en 18 cuotas de u$d${(Math.round((precioFinal / 18) * 1.21))}`;
-installments30.innerHTML = `Paga en 30 cuotas de u$d${(Math.round((precioFinal / 30) * 1.21))}`;
-
+let installments = document.querySelectorAll('.installments');
+for (let i = 0; i<installments.length; i++){
+    installments[i].innerHTML = `Paga en ${installments[i].value} cuotas de u$d${Math.round((precioFinal/parseInt(installments[i].value))*1.21)}`;
+}
 let namePerson = document.getElementById('namePerson');
 let surname = document.getElementById('surnamePerson');
 let dirPerson = document.getElementById('dirPerson');
